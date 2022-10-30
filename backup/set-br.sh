12105cc0ce100cbc2a6ea2afa602e36b5ec530cb
lesta-2 <39772702+lesta-2@users.noreply.github.com>
2022-10-30 13:39:47 +0700

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+# Link Hosting Kalian
+wisangvpn="raw.githubusercontent.com/lesta2/install/main/backup"
+
+apt install rclone -y
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/lesta-2/install/main/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user bckupvpns@gmail.com
+from bckupvpns@gmail.com
+password Yangbaru1 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://raw.githubusercontent.com/lesta-2/install/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/lesta-2/install/main/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/lesta-2/install/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/lesta-2/install/main/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/lesta-2/install/main/limitspeed.sh"
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
